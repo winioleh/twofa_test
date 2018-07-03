02fcd0c0b7d0fe8a85f244c4ba40f2745b61030a
unknown <winioleh98@gmail.com>
2018-07-03 15:29:40 +0300

    LAST

--- first_task/urls.py
+++ first_task/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
@@ -23,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("twofa_test.urls"))
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
